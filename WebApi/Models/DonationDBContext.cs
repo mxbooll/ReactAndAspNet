2484c84aac113b72bf35c93d24cf405eca7cd2da
mxbooll <mxbooll@gmail.com>
2020-04-05 21:07:23 +0500

    WebApi comlete

---
+++ WebApi/Models/DonationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApi.Models
+{
+    public class DonationDBContext : DbContext
+    {
+        public DonationDBContext(DbContextOptions<DonationDBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<DCandidate> DCandidates { get; set; }
+    }
+}
